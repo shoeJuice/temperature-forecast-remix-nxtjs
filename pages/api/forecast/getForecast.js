dd3826be6d068153fa42b7627f9764057fbc1007
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-05-21 13:15:15 -0400

    Changes: - Set display container to be reactive to changes in location - Added icons for more weather states

--- pages/api/forecast/getForecast.js
+++ pages/api/forecast/getForecast.js
@@ -6,15 +6,14 @@ export default async function handler(req, res) {
     //Record query parameters
     const params = req.query
     
+    const responseObject = {data: null, city: null}
     
-    const response = await axios.get(`https://pro.openweathermap.org/data/2.5/onecall?lat=${params.lat}&lon=${params.lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)
-    const currentCity = await axios.get(`https://pro.openweathermap.org/data/2.5/forecast/daily?lat=${params.lat}&lon=${params.lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)
+    const response = await axios.get(`https://pro.openweathermap.org/data/2.5/onecall?lat=${params.lat}&lon=${params.lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}&units=imperial`).then((res) => {responseObject.data=res.data})
+    const currentCity = await axios.get(`https://pro.openweathermap.org/data/2.5/forecast/daily?lat=${params.lat}&lon=${params.lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_API_KEY}&units=imperial`).then((res) => {responseObject.city=res.data.city.name})
 
     
-    return(res.status(200).send({
-        data: response.data,
-        city: currentCity.data.city.name
-    })) 
+    
+    return(res.status(200).send(responseObject)) 
 
     res.status(404).send("INTERNAL SERVER ERROR 404")
   }
